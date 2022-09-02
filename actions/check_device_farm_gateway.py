699fa0dabea458c815fd80cc2c6d6041579b94a5
circle-jin <wonjin.shin@nota.ai>
2022-09-02 18:37:33 +0900

    update to check device_farm_gateway

--- actions/check_device_farm_gateway.py
+++ actions/check_device_farm_gateway.py
@@ -29,7 +29,7 @@ def notify_device_farm_gateway_shutdown_with_slack():
     webhook_url = "https://hooks.slack.com/services/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"
     msg_string = "[NOTICE] device_farm_gateway"
     msg = {"text": f"{msg_string}"}
-    requests.post(webhook_url, data=json.dumps(msg), headers={'Content-Type': 'application/json'})
+    requests = requests.post(webhook_url, data=json.dumps(msg), headers={'Content-Type': 'application/json'})
     print(requests.raise_for_status())
     print("END")
 
