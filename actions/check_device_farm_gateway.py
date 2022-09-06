d620e3da4201639f77f382ed92f3a3ec7d847c99
circle-jin <wonjin.shin@nota.ai>
2022-09-06 13:05:07 +0900

    fix: update git action

--- actions/check_device_farm_gateway.py
+++ actions/check_device_farm_gateway.py
@@ -12,18 +12,16 @@ def check_device_farm_gateway_status(gateway_url):
         string : Status_code of the device_farm_gateway
     """
     try:
+        device_farm_gateway_status_code = "Exception in request.get"
         device_farm_gateway = requests.get(gateway_url)
-        result = str(device_farm_gateway.status_code)
-        if result != "200":
-            print("Device_Farm API Error : %s" %gateway_url)
-            return False, result
-        return True, result
+        device_farm_gateway_status_code = str(device_farm_gateway.status_code)
+        return device_farm_gateway_status_code
     except Exception as e:
         print('Exception : ', e)
         print(traceback.format_exc())
     finally:
         device_farm_gateway.close()
-    return None
+    return device_farm_gateway_status_code
 
 def notify_device_farm_gateway_with_slack(gateway_slack_webhook_url, msg_string):
     """Notify Slack when the device_farm_gateway server is shut down
@@ -36,13 +34,16 @@ def notify_device_farm_gateway_with_slack(gateway_slack_webhook_url, msg_string)
 def main():
     DEVICE_FARM_GATEWAY_URL = os.environ.get("DEVICE_FARM_GATEWAY_URL")
     GATEWAY_SLACK_WEBHOOK_URL = os.environ.get("GATEWAY_SLACK_WEBHOOK_URL")
-     
-    print(DEVICE_FARM_GATEWAY_URL)
-    device_farm_gateway_status, device_farm_gateway_status_code = check_device_farm_gateway_status(DEVICE_FARM_GATEWAY_URL)
 
-    if device_farm_gateway_status:
+    DEVICE_FARM_GATEWAY_URL = "http://10.169.1.100:8888/health"
+    GATEWAY_SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
+     
+    device_farm_gateway_status_code = check_device_farm_gateway_status(DEVICE_FARM_GATEWAY_URL)
+
+    if device_farm_gateway_status_code == "200":
         print("[NOTICE] device_farm_gateway is active!, STATUS_CODE : " + device_farm_gateway_status_code)
     else:
+        print("Device_Farm API Error : %s" %device_farm_gateway_status_code)
         msg_string = "*[NOTICE]* device_farm_gateway is *inactive*!\n :black_medium_small_square: STATUS_CODE : " + device_farm_gateway_status_code
         notify_device_farm_gateway_with_slack(GATEWAY_SLACK_WEBHOOK_URL, msg_string)
     
